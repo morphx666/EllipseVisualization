07ffd1179c62532c4c88d77b17f839eed67606af
Xavier Flix <xavier@xfx.net>
2018-08-29 20:09:15 -0400

    Added point/segment extensions

---
+++ EllipseVisualization/Extensions.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module Extensions
+    <Extension()>
+    Public Function Plus(p1 As PointF, p2 As PointF) As PointF
+        Return New PointF(p1.X + p2.X, p1.Y + p2.Y)
+    End Function
+
+    <Extension()>
+    Public Function Minus(p1 As Point, p2 As PointF) As PointF
+        Return New Point(p1.X - p2.X, p1.Y - p2.Y)
+    End Function
+
+    <Extension()>
+    Public Function Minus(p1 As PointF, p2 As PointF) As PointF
+        Return New PointF(p1.X - p2.X, p1.Y - p2.Y)
+    End Function
+
+    <Extension()>
+    Public Function Mult(p1 As PointF, v As Single) As PointF
+        Return New PointF(p1.X * v, p1.Y * v)
+    End Function
+
+    <Extension()>
+    Public Function Distance(p1 As PointF, p2 As PointF) As Single
+        Return Math.Sqrt((p2.X - p1.X) ^ 2 + (p2.Y - p1.Y) ^ 2)
+    End Function
+
+    <Extension()>
+    Public Function Slope(p1 As PointF, p2 As PointF) As Single
+        Return (p2.Y - p1.Y) / (p2.X - p1.X)
+    End Function
+
+    <Extension()>
+    Public Function IsEqual(p1 As PointF, p2 As PointF) As Boolean
+        Return p1.X = p2.X AndAlso p1.Y = p2.Y
+    End Function
+
+    <Extension()>
+    Public Function InLine(p As PointF, p1 As PointF, p2 As PointF) As Boolean
+        If p1.X <> p2.X Then
+            If p1.X <= p.X AndAlso p.X <= p2.X Then Return True
+            If p1.X >= p.X AndAlso p.X >= p2.X Then Return True
+        Else
+            If p1.Y <= p.Y AndAlso p.Y < p2.Y Then Return True
+            If p1.Y >= p.Y AndAlso p.Y >= p2.Y Then Return True
+        End If
+
+        Return False
+    End Function
+
+    <Extension()>
+    Public Function Dot(p1 As PointF, p2 As PointF) As Single
+        Return p1.X * p2.X + p1.Y * p2.Y
+    End Function
+
+    <Extension()>
+    Public Function Perp(p1 As PointF, p2 As PointF) As Single
+        Return p1.X * p2.Y + p1.Y * p2.X
+    End Function
+End Module
--- EllipseVisualization/FormMain.vb
+++ EllipseVisualization/FormMain.vb
@@ -61,9 +61,7 @@
     Private Sub HandleMouseMove(e As MouseEventArgs)
         If mouseIsDown Then
             Select Case overSection
-                Case OverSections.EccentricPoint
-                    ep.X += e.X - mouseOrigin.X
-                    ep.Y += e.Y - mouseOrigin.Y
+                Case OverSections.EccentricPoint : ep = ep.Plus(e.Location.Minus(mouseOrigin))
                 Case OverSections.EllipseLeft : w -= e.X - mouseOrigin.X
                 Case OverSections.EllipseRight : w += e.X - mouseOrigin.X
                 Case OverSections.EllipseTop : h -= e.Y - mouseOrigin.Y
@@ -112,8 +110,10 @@
         Dim p1 As PointF
         Dim p2 As PointF
         Dim cp As PointF
+        Dim a As Single
+
         Using p As New Pen(Color.FromArgb(128, Color.Gray))
-            For a As Single = 0 To Math.PI * 2 Step Math.PI / 60
+            For a = 0 To Math.PI * 2 Step Math.PI / 60
                 p2.X = w / 2 * Math.Cos(a)
                 p2.Y = -h / 2 * Math.Sin(a)
                 g.DrawLine(p, ep, p2)
@@ -130,18 +130,93 @@
         End Using
         DrawEllipseFromCenter(g, Pens.Yellow, ep.X, ep.Y, 10, 10, True)
 
+        ' TODO: generate the inner ellipse's formula and draw it.
+        '           I guess I'll have to re-watch the video...
+        'p1 = New PointF(0, 0)
+        'g.DrawLine(Pens.Red, p1, ep)
+        'a = Math.Atan(p1.Slope(ep))
+        'cp = GetCenter(p1, ep)
+
+        'DrawEllipseFromCenter(g, Pens.Red, 0, 0, 10, 10, True)
+
+        'g.ResetTransform()
+        'g.TranslateTransform(ecp.X + cp.X, ecp.Y + cp.Y)
+        'g.RotateTransform(a * 180 / Math.PI)
+        'Using p As New Pen(Color.Magenta, 2)
+        '    DrawEllipseFromCenter(g, p, 0, 0, ecp.X / 2, ecp.Y / 2)
+        'End Using
+
         g.ResetTransform()
         g.DrawString($"Use mouse to resize Ellipse{Environment.NewLine}and move point", Me.Font, Brushes.Gray, 10, 10)
     End Sub
+
+    ' http://geomalgorithms.com/a05-_intersect-1.html#intersect2D_2Segments()
+    Private Function GetIntersection(s10 As PointF, s11 As PointF, s20 As PointF, s21 As PointF) As PointF
+        Dim u As PointF = s11.Minus(s10)
+        Dim v As PointF = s21.Minus(s20)
+        Dim w As PointF = s10.Minus(s20)
+        Dim d As Single = u.Perp(v)
+
+        If Math.Abs(d) < 0.001 Then ' The lines are parallel
+            If u.Perp(w) <> 0 OrElse v.Perp(w) <> 0 Then Return Point.Empty
+
+            Dim du As Single = u.Dot(u)
+            Dim dv As Single = v.Dot(v)
+
+            If du = 0 OrElse dv = 0 Then
+                If Not s10.Equals(s20) Then Return PointF.Empty
+                Return s10
+            End If
+
+            If du = 0 Then
+                If Not s10.InLine(s20, s21) Then Return Point.Empty
+                Return s10
+            End If
+            If dv = 0 Then
+                If Not s20.InLine(s10, s11) Then Return Point.Empty
+                Return s20
+            End If
+
+            Dim t0 As Single
+            Dim t1 As Single
+            Dim w2 As PointF = s11.Minus(s20)
+            If v.X <> 0 Then
+                t0 = w.X / v.X
+                t1 = w2.X / v.X
+            Else
+                t0 = w.Y / v.Y
+                t1 = w2.Y / v.Y
+            End If
+            If t0 > t1 Then
+                Dim t As Single = t0
+                t0 = t1
+                t1 = t0
+            End If
+            If t0 > 1 OrElse t1 < 0 Then Return Point.Empty
+            t0 = If(t0 < 0, 0, t0)
+            t1 = If(t1 > 1, 1, t1)
+            If t0 = t1 Then Return s20.Plus(v.Mult(t0))
+
+            s20.Plus(v.Mult(t0))
+        End If
+
+        Dim si As Single = v.Perp(w) / d
+        If si < 0 OrElse si > 1 Then Return Point.Empty
+
+        Dim ti As Single = u.Perp(w) / d
+        If ti < 0 OrElse ti > 1 Then Return Point.Empty
+
+        Return s10.Plus(u.Mult(si))
+    End Function
 
     Private Function GetCenter(p1 As PointF, p2 As PointF) As PointF
         Return New PointF(p1.X + (p2.X - p1.X) / 2, p1.Y + (p2.Y - p1.Y) / 2)
     End Function
 
     Private Function RotateLineAtPoint(p1 As PointF, p2 As PointF, angle As Double) As PointF
-        Dim p3 As PointF = New Point(p2.X - p1.X, p2.Y - p1.Y)
+        Dim p3 As PointF = p1.Minus(p2)
         p3 = RotateLineAtOrigin(p3, angle)
-        Return New PointF(p2.X + p3.X, p2.Y + p3.Y)
+        Return p2.Plus(p3)
     End Function
 
     Private Function RotateLineAtOrigin(p As PointF, angle As Double) As PointF
